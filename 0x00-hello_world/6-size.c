501eeebd9bffddfcd87c6aff08d27fbef6ec960b
Goodstar Ohia <goodstarohia@gmail.com>
2023-03-19 19:43:42 -0700

    Made some corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,14 @@ int main(void)
 {
 char c;
 int i;
+long int li;
+long long int lli;
 float f;
 /*Signed and unsigned sizes*/
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %li byte(s)\n", (long)sizeof(i));
-printf("Size of a long long int: %lli byte(s)\n", (long long)sizeof(i));
+printf("Size of a long int: %li byte(s)\n", (long)sizeof(li));
+printf("Size of a long long int: %lli byte(s)\n", (long long)sizeof(lli));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
