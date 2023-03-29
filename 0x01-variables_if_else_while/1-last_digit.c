d67cef89198fc90d45afbf8a0245cbe77a0da2b4
Goodstar Ohia <goodstarohia@gmail.com>
2023-03-28 20:18:38 -0700

    Made correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,11 +26,11 @@ int main(void)
 	{
 		printf(" and is greater than 5\n");
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		printf(" and is 0\n");
 	}
-	if (n < 6 != 0)
+	else
 	{
 		printf(" and is less than 6 and not 0\n");
 	}
