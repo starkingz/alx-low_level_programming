e7a9724b8b9f3d3a6d0aee9033015e44c8402531
Goodstar Ohia <goodstarohia@gmail.com>
2023-03-30 16:21:35 -0700

    Added a new file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program'
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char lowercase_alpha = 'a';
+	char uppercase_alpha = 'A';
+
+	/* Prints the alphabet in lowercase, and then in uppercase */
+	for (lowercase_alpha = 'a'; lowercase_alpha <= 'z'; lowercase_alpha++)
+		putchar(lowercase_alpha;
+
+	for (uppercase_alpha = 'A'; uppercase_alpha <= 'Z'; uppercase_alpha++)
+		putchar(uppercase_alpha);
+
+	/* Followerd by a new line */
+	putchar('\n');
+
+	return (0);
+}
