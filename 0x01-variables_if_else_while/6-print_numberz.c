1929de7d4023c08dd4a5f1720800055e06ac3ebf
Goodstar Ohia <goodstarohia@gmail.com>
2023-04-10 06:50:13 -0700

    upload files

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description: a simple program that prints all single digit numbers of
+ * base 20 starting from '0'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n++);
+	}
+	putchar('\n'); /* end of line */
+
+		return (0);
+}
