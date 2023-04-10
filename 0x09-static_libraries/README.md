74f74a5be8dd2ffc895c9bfa02e5efe0a51718d6
Goodstar Ohia <81324138+starkingz@users.noreply.github.com>
2023-04-10 17:32:06 -0400

    Create library with previous functions

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-## 0x09-static_libraries
+Root directory for static libraries
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-/* MAIN_H */
-
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
@@ -22,5 +20,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
+
