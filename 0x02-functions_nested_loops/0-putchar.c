873495ab908662357ec878cc12a7b3fd777e0ac6
Goodstar Ohia <goodstarohia@gmail.com>
2023-04-26 23:16:52 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Description: a program taht prints '_putchar'
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
