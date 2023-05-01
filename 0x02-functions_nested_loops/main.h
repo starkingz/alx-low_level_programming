ba3a2406a978cee14a1ebf1aae8c8650b93787e9
Goodstar Ohia <goodstarohia@gmail.com>
2023-04-30 22:56:32 -0700

    add a file that prints an alphabet

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: 0 Always
+ */
+void print_alphabet(void);
