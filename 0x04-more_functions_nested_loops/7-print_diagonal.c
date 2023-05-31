5e25cd45ae7cc0ad9a7f2cbb41a07dd29e53954b
Goodstar Ohia <goodstarohia@gmail.com>
2023-05-30 23:45:24 -0700

    updatw 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,13 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
+	int i = 0, j;
 
 	if (n <= 0)
 		_putchar('\n');
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 1; j < i; j++)
 		{
 			_putchar(32);
 		}
