369bbb203560f95bf02876471bd606de28b6a4aa
Goodstar Ohia <goodstarohia@gmail.com>
2023-06-04 11:38:17 -0700

    added Fizz-Buzz test file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poinr
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	for (n = 1; n <= 100; ++n)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+	}
+	putchar('\n');
+	return (0);
+}
