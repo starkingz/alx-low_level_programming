bf0696512a7e5781aa4df863528d617e64fb0ce4
Goodstar Ohia <goodstarohia@gmail.com>
2023-06-04 11:44:40 -0700

    update 9-fizz-buzz.c file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else if (n % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz");
 		else
-			printf("%d ", n);
+			printf(" %d", n);
 	}
 	putchar('\n');
 	return (0);
