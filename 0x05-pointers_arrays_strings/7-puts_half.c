2aa4a89aa1e73d4441dce26b44aeced178e6ae91
starkingz <goodstarohia@gmail.com>
2023-06-14 19:44:54 -0700

    corrected file and fixed betty style coding

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,14 +14,15 @@ void puts_half(char *str)
 	int len = 0;
 
 	len = strlen(str);
-	n = (strlen(str) - 1) / 2;
+	n = (strlen(str) + 1) / 2;
 
 	/* If length of char is odd print starting from n */
 	if (strlen(str) % 2 == 1)
 	{
-		for (i = n; i <= len - 1; i++);
+		for (i = n; i <= len - 1; i++)
 		_putchar(str[i]);
-	} else {
+	} else
+	{
 		for (i = len / 2; i <= len - 1; i++)
 			_putchar(str[i]);
 	}
