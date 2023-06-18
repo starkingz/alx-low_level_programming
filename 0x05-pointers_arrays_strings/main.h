f86856167c6b039ce14658058a744ec30207cfd8
starkingz <goodstarohia@gmail.com>
2023-06-18 16:11:02 -0700

    added  header file for 8-print_array.c function

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -66,3 +66,11 @@ void puts2(char *s);
  *
  */
 void puts_half(char *str);
+
+/**
+ * print_array - prints 'n' elements of an array of integers
+ * @a: input of array
+ * @n: elements of array
+ *
+ */
+void print_array(int *a, int n);
