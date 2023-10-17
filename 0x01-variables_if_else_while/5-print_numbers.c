763d5c1d530e8267d57b7ecd3c56f2dcde1def49
starking <stargeniux@gmail.com>
2023-10-16 19:58:12 -0700

    added 5-print_numbers.c + exec

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - entry point
- * Description: a simple program that prints all single diigt of base 10
- * starting from '0'
- * Return: Always 0 (success)
+ * main - print digit from 0 to 9
+ *
+ * Return: always 0 (success)
  */
-
 int main(void)
 {
-char n = 0;
+	int n = 0;
 
-while (n <= 9)
-{
-printf("%d", n++);
-}
-printf("\n"); /* end of line */
+	while (n < 10)
+	{
+		printf("%d", n);
+		++n;
+	}
+	printf("\n");
 
-return (0);
+	return (0);
 }
