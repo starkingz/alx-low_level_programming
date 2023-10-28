7a6d2cea4dc92d69857d82994cddbd79693e0e03
starkingz <goodstarohia@gmail.com>
2023-10-28 03:17:38 +0000

    fix several bugs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,40 @@
 #include "main.h"
-
 /**
  * print_times_table - print input times table starting with 0
  * @n: argument
- *
  * Return: void
  */
 void print_times_table(int n)
 {
-	int mul;
-	int mulcand;
-	int prod = 0;
-	int div = 0;
-	int mod = 0;
-	if (n < 15 && n > 0)
-	{
+	int mul, mulcand;
+
+if (n <= 15 && n >= 0)
+{
 	for (mul = 0; mul <= n; ++mul)
 	{
-		for (mulcand = 0; mulcand <= n; ++mulcand)
+		_putchar('0');
+		for (mulcand = 1; mulcand <= n; ++mulcand)
 		{
-			prod = mulcand * mul;
-			div = prod / 10;
-			mod = prod % 10;
+			int prod = mulcand * mul;
+			int div = prod / 10;
+			int mod = prod % 10;
 
+			_putchar(',');
+			_putchar(' ');
 			if (prod <= 9)
 			{
-				if (mulcand > 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(mod + '0');
 			}
-			else if (prod > 9 && prod <= 99)
+			else if (prod <= 99)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 				_putchar(div + '0');
 				_putchar(mod + '0');
-		        }
+			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(div / 10 + '0');
 				_putchar(div % 10 + '0');
 				_putchar(mod + '0');
@@ -53,5 +42,5 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
-	}
+}
 }
