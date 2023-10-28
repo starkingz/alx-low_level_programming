47ab118f8aceef1b53eef18bb70cf62eedf28d65
starkingz <goodstarohia@gmail.com>
2023-10-28 02:33:55 +0000

    fix bug

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,13 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 
 	if (last_digit < 0)
+	{
 		_putchar(-last_digit + '0');
+		return (-last_digit);
+	}
 	else
+	{
 		_putchar(last_digit + '0');
-	return (-last_digit);
+		return (last_digit);
+	}
 }
