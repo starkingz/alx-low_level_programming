d20c2af02225ab4e4d0eea5feff0ebb97072c16b
starkingz <goodstarohia@gmail.com>
2024-01-20 12:59:51 +0000

    add knowaddress.c

---
+++ test/knowaddress.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - addresses of variables
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	printf("Address of variable 'c' is %p\n", &c);
+	printf("Address of variable 'i' is %p\n", &i);
+
+	return (0);
+}
