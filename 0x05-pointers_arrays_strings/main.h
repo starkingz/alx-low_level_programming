01211f2a2ed1bda07773a704ae637531100143cb
starkingz <goodstarohia@gmail.com>
2024-02-10 17:23:44 +0000

    add swap_int prototype

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,3 +11,11 @@ int _putchar(char c);
  * @n: pointer
  */
 void reset_to_98(int *n);
+
+/**
+ * swap_int - swap values of two variables using deferencing
+ * @a: first pointer
+ * @b: second pointer
+ *
+ */
+void swap_int(int *a, int *b);
