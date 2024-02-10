f56882aab4a64b45712e8c5c7488ae2f8c75dd85
starkingz <goodstarohia@gmail.com>
2024-02-10 19:58:35 +0000

    add strlen prototype

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -19,3 +19,11 @@ void reset_to_98(int *n);
  *
  */
 void swap_int(int *a, int *b);
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s);
