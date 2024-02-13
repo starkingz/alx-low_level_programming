293e542949ecdb5c1fe8f31f842cc41d57e5cecd
starkingz <goodstarohia@gmail.com>
2024-02-13 06:07:38 +0000

    add main for 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/* Test cases */
+	char *str;
+	char *s1, *s2, *s3;
+
+	str = "Whatever people may think of you do that which you believe to be right - PYTHAGORAS";
+	_puts(str);
+
+	s1 = "Some are born great, some achieve greatness and some have greatness thrust upon them - SHAKESPEARE";
+	_puts(s1);
+
+	s2 = "If you are satisfied just getting by step aside for the person who isn't - A. G. GASTON";
+	_puts(s2);
+
+	s3 = "If you set yourself on fire the world will come to see you burn - DON KING";
+	_puts(s3);
+	return (0);
+}
