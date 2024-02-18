a8c38d345a1effe1d635720d0e650d3537e31bc7
starkingz <goodstarohia@gmail.com>
2024-02-18 00:12:18 +0000

    add 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * Description: copy including the terminating null byte ('\0')
+ * @dest: char to return
+ * @src: value to copy
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = strlen(src);
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
