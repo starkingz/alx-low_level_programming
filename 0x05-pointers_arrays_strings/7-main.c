b650c6ffe502560555e2350e5f884a7d291319af
starkingz <goodstarohia@gmail.com>
2024-02-23 20:17:15 +0000

    main for 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -8,8 +8,11 @@
 int main(void)
 {
 	char *str;
+	char *s;
 
 	str = "0123456789";
 	puts_half(str);
+	s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	puts_half(s);
 	return (0);
 }
