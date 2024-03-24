b6f2b662792f39277cbc487c290c283956b3ab8b
starkingz <goodstarohia@gmail.com>
2024-03-24 22:11:47 +0000

    add reverse_array prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int _putchar(char c);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -7,3 +9,5 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
