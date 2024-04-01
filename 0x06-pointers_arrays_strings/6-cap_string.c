f1bc06973e1e02d3ea4566622f0ffe35eb2aa822
starkingz <goodstarohia@gmail.com>
2024-04-01 09:30:32 +0000

    fix first char not capitalizing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,31 +11,27 @@
 char *cap_string(char *s)
 {
 	int i, cp = 0, len = 0;
-	char *ch;
 
 	len = strlen(s);
-	ch = (char *) malloc(len * sizeof(char));
-
-	for (i = 0; i < len; i++)
-		ch[i] = s[i];
-
 	for (i = 0; i < len; i++)
 	{
-		cp = ch[i - 1];
+		cp = s[i - 1];
+		if ( i == 0 && s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
 		switch (cp)
 		{
 		case 32: case 9: case 10: case 44: case 59: case 46:
 		case 33: case 63: case 34: case 40: case 41: case 123:
 		case 125:
 		{
-			if (ch[i] >= 97 && ch[i] <= 122)
-				s[i] = ch[i] - 32;
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] = s[i] - 32;
 			else
-				s[i] = ch[i];
+				s[i] = s[i];
 		}
 		break;
 		default:
-			s[i] = ch[i];
+			s[i] = s[i];
 		}
 	}
 	return (s);
