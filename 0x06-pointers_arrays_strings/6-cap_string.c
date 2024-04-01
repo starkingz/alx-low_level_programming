baa9583e145ed7e9fd17deb9df1b0219039673f3
starkingz <goodstarohia@gmail.com>
2024-04-01 09:03:20 +0000

    fix bug

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * cap_string - capitalizes all firt word of a string
@@ -10,26 +11,32 @@
 char *cap_string(char *s)
 {
 	int i, cp = 0, len = 0;
+	char *ch;
 
 	len = strlen(s);
+	ch = (char *) malloc(len * sizeof(char));
+
+	for (i = 0; i < len; i++)
+		ch[i] = s[i];
+
 	for (i = 0; i < len; i++)
 	{
-		cp = s[i - 1];
+		cp = ch[i - 1];
 		switch (cp)
 		{
 		case 32: case 9: case 10: case 44: case 59: case 46:
 		case 33: case 63: case 34: case 40: case 41: case 123:
 		case 125:
 		{
-			if (s[i] >= 97 && s[i] <= 122)
-				s[i] = s[i] - 32;
+			if (ch[i] >= 97 && ch[i] <= 122)
+				s[i] = ch[i] - 32;
 			else
-				s[i] = s[i];
+				s[i] = ch[i];
 		}
 		break;
 		default:
-			s[i] = s[i];
+			s[i] = ch[i];
 		}
 	}
-	return s;
+	return (s);
 }
